027d9e144ee59d90e7ad86dd5fa3194cb1e3643e
aehmt <calisahmet@gmail.com>
2016-10-27 14:07:40 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 const app = "I don't do much.";
+function multiplier(a, b) {
+  return a * b;
+}
+
+
+const doubler = multiplier.bind(null, 2);
+ 
+// Now we can use doubler like this:
+console.log(doubler(5)); // prints 10
